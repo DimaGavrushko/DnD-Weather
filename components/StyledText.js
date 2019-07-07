e1d0c84ba4582e72408d1a0cea07c2197600d8c9
Dmitry <gavrushko98@gmail.com>
2019-07-07 17:13:36 +0300

    fixed bug with font

--- components/StyledText.js
+++ components/StyledText.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Platform, Text} from 'react-native';
+import {Text} from 'react-native';
 
 export function MonoText(props) {
     return (
-        <Text {...props} style={[props.style, {fontFamily: Platform.OS === 'San-Francisco' ? '' : 'Roboto'}]}/>
+        <Text {...props} style={[props.style, {fontFamily: 'roboto-regular' }]}/>
     );
 }
